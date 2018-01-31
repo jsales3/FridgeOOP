d946a3a21895eb77e3552b63849efd9a7ea939d2
jsales3 <jsales3>
2018-01-31 14:08:02 -0500

    Initial commit

---
+++ FridgeOOP/Fridge.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeOOP
+{
+    public class Fridge
+    {
+        //fields
+        private bool isClean;
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+
+        //Properties
+        public bool IsClean
+        {
+            get { return this.isClean; }
+            set { this.isClean = value; }
+        }
+
+        public string DispenserType
+        {
+            get { return this.dispenserType; }
+            
+        }
+
+        public int FoodAmount
+        {
+            get { return this.foodAmount; }
+            set { this.foodAmount = value; }
+        }
+
+        public bool BulbsWork
+        {
+            get { return this.bulbsWork; }
+            set { this.bulbsWork = value; }
+        }
+
+
+
+        //Constructors
+
+        //Methods()
+    }
+}
