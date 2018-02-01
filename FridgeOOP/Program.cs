ff8fbb9a383a02ea6a61b7d9ed4f9661b7ad9fec
jsales3 <jsales3>
2018-02-01 11:16:26 -0500

    Added switch statment

--- FridgeOOP/Program.cs
+++ FridgeOOP/Program.cs
@@ -15,6 +15,7 @@ namespace FridgeOOP
             //call methods on fridge object to so what user wants
 
             int userChoice;
+            Fridge userFridge = new Fridge(false, "deluxe dispenser", 70, true);
 
             do
             {
@@ -27,10 +28,48 @@ namespace FridgeOOP
                 Console.WriteLine("to take food, type 4");
                 Console.WriteLine("To check food supples, type 5");
                 Console.WriteLine("To see all fridge detail, type 6");
-                Console.WriteLine("To restart, type 7");
+                //Console.WriteLine("To restart, type 7");
                 Console.WriteLine("To quit, type 8");
 
                 userChoice = int.Parse(Console.ReadLine());
+
+
+                
+
+                switch (userChoice)
+                {
+                    case 1:
+                        userFridge.DispenseWater();
+                        break;
+
+                    case 2:
+                        Console.WriteLine(userFridge.ChangeBulbs());
+                        break;
+
+                    case 3:
+                        Console.WriteLine(userFridge.Clean());
+                        break;
+
+                    case 4:
+                        Console.WriteLine("How much food would you like to remove");
+                        int removeFood = int.Parse(Console.ReadLine());
+                        userFridge.RemoveFood(removeFood);
+                        Console.WriteLine("your new new food level is: " + userFridge.FoodAmount);
+                       break;
+
+                    case 5:
+                        Console.WriteLine(userFridge.CheckSupplies());
+                        break;
+
+                    case 6:
+                        Console.WriteLine("your fridge detials is: " + userFridge.IsClean + "your fridge is clean" + userFridge.DispenserType + 
+                            userFridge.FoodAmount + userFridge.BulbsWork + "you bulbs work");
+                        break;
+
+
+
+
+                }
 
             } while (userChoice != 8);
         }
